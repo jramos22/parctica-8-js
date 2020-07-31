9b5b3d9c2be1cd6919e275b5667ab05939ea9bb4
José Daniel Ramos Barquero <josedanielramosbarquero@gmail.com>
2020-07-31 06:58:52 -0600

    agrego tarea numero 8 de JS

---
+++ index.js
@@ -0,0 +1,23 @@
+const tareas = ['Practica de Javascript','Ir al supermercado','Pasear al perro','Lavar los trastos'];
+
+
+const contenedor = document.getElementById('contenedor');
+const lista = document.createElement('ul');
+contenedor.appendChild(lista);
+for (let i = 0; i < tareas.length; i++) {
+    //creo y agrego lista
+    const listainterna = document.createElement('li');
+    lista.appendChild(listainterna);
+    //creo, agrego valores y introdusco a input dentro de li 
+    const input = document.createElement('input');
+    input.setAttribute('type','checkbox');
+    input.setAttribute('id',`tarea-${tareas[i]}`);
+    listainterna.appendChild(input);
+    //creo, agrego valores y introdusco a labal dentro de li
+    const label = document.createElement('label');
+    label.setAttribute('for',`tarea-${tareas[i]}`);
+    //agrego el texto que va dentro de label    
+    label.innerHTML = tareas[i];
+    listainterna.appendChild(label);
+    
+}
